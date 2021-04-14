b07dbafdd3d85e7b294ea388e642726f80463cba
John Kotopka <jkotopka@gmail.com>
2021-04-14 09:21:24 -0500

    update BruteForceSubstringSearch.java add more test cases

--- src/org/kotopka/BruteForceSubstringSearch.java
+++ src/org/kotopka/BruteForceSubstringSearch.java
@@ -3,16 +3,16 @@ package org.kotopka;
 public class BruteForceSubstringSearch {
 
     public static int search(String text, String pattern) {
-        int textLen = text.length();
-        int pattLen = pattern.length();
+        int N = text.length();
+        int M = pattern.length();
 
-        for (int i = 0; i < textLen; i++) {
+        for (int i = 0; i <= N - M + 1; i++) {
             int j;
-            for (j = 0; j < pattLen; j++) {
+            for (j = 0; j < M; j++) {
                 if (text.charAt(i + j) != pattern.charAt(j)) break;
             }
 
-            if (j == pattLen) return i;
+            if (j == M) return i;
         }
 
         return -1; // not found
@@ -24,5 +24,7 @@ public class BruteForceSubstringSearch {
 
         System.out.println("Index of pattern 'needle': " + search(text, pattern));
         System.out.println("Index of pattern 'horse': " + search(text, "horse"));
+        System.out.println("Index of pattern 'eina': " + search(text, "eina"));
+        System.out.println("Index of pattern 'inah': " + search(text, "inah"));
     }
 }
